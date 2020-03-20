30c97d936769f45dd542af52b9048baed760d2cc
Vadim Samarin <dezivert@gmail.com>
2020-03-20 16:23:14 +0200

    Docker config

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,4 @@
+const config = require('config');
 const initServer = require('./http').init;
 const initDb = require('./db').init;
 
@@ -6,7 +7,7 @@ const initDb = require('./db').init;
   await initDb();
 
   try {
-    await initServer({ port: 4000 });
+    await initServer({ port: config.get('port') || 4000 });
   } catch (e) {
     if (e.code === 'EADDRINUSE') {
       console.log('You might wanna use different port as this one is taken.');
